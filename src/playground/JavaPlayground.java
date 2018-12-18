37bb9e2b4a5159af924c692cbc46eadddacf5a3e
tomparkpro <tomparkpro@gmail.com>
2018-12-18 22:51:44 +0100

    first scala lecture

---
+++ src/playground/JavaPlayground.java
@@ -0,0 +1,16 @@
+package playground;
+
+/**
+ * Created by TomPark on 2018-12-18
+ *
+ * @author tomparkpro@gmail.com
+ * github : http://github.com/tomparkpro
+ */
+public class JavaPlayground {
+
+    public static void main(String args[]) {
+
+        System.out.println("Hello, Java!");
+
+    }
+}
