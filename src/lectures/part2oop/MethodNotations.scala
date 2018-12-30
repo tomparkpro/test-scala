326598898341c9743054ee58f45951792cb390a2
tomparkpro <tomparkpro@gmail.com>
2018-12-30 17:39:26 +0100

    lecture13. Method Notations (Exercises)

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -2,12 +2,17 @@ package lectures.part2oop
 
 object MethodNotations extends App {
 
-  class Person(val name:String, favoriteMovie: String) {
+  class Person(val name:String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(nickname: String): Person = new Person(s"$name ($nickname)", favoriteMovie)
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
+    def learns(subject: String): String = s"$name learns $subject"
+    def learnsScala: String = this learns "Scala"
     def unary_! : String = s"$name, what the heck?!"
     def isAlive: Boolean = true
     def apply(): String = s"Hi, my name is $name I like $favoriteMovie"
+    def apply(n: Int) = s"$name watched $favoriteMovie $n times"
   }
 
   val mary = new Person("Mary", "Inception")
@@ -39,4 +44,25 @@ object MethodNotations extends App {
   // apply
   println(mary.apply())
   println(mary()) // equivalent
+
+  println((mary + "the rockstar").apply())
+
+  println((+mary).age)
+  println(mary learnsScala)
+  println(mary(10))
+  /*
+    1. overload the + operator
+      mary + "the rockstar" => new person "Mary (the rockstar)"
+
+    2. add an age to the Person class
+       add a unary + operator => new Person with the age + 1
+       +mary => mary with the age incrementer
+
+    3. add a "learns" method in the Person class => "Mary learns Scala"
+       add a learnsScala method, calls learns method with "Scala"
+        use it in postfix notation.
+
+    4. overload the apply method
+       mary.apply(2) => "Mary watched Inception 2 times"
+   */
 }
